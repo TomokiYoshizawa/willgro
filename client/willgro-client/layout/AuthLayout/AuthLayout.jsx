3a87856125872a6f5b8ceba1698b38820ce6207e
Tomoki Yoshizawa <tomoki-goalie@hotmail.com>
2024-03-15 18:31:03 -0700

    add redux

--- client/willgro-client/layout/AuthLayout/AuthLayout.jsx
+++ client/willgro-client/layout/AuthLayout/AuthLayout.jsx
@@ -1,10 +1,24 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import willgroLogo from "../../src/assets/image/willgro.jpg";
 
 import "./AuthLayout.scss";
+import authUtils from "../../src/utils/atuhUtils";
+import { useEffect } from "react";
 
 function AuthLayout() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    // Redirect to home page if user is already logged in
+    const checkAuth = async () => {
+      const isAuth = await authUtils.isAuthenticated();
+      if (isAuth) {
+        navigate("/");
+      }
+    };
+    checkAuth();
+  }, [navigate]);
+
   return (
     <div className="auth">
       <div className="auth__wrapper">
--- client/willgro-client/src/api/authApi.jsx
+++ client/willgro-client/src/api/authApi.jsx
@@ -3,6 +3,7 @@ import axiosClient from "./axiosClient";
 const authApi = {
   register: async (params) => axiosClient.post("/auth/register", params),
   login: async (params) => axiosClient.post("/auth/login", params),
+  verifyToken: () => axiosClient.post("/auth/verify-token"),
 };
 
 export default authApi;
