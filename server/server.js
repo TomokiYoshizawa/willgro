31ab6855e445d2d54e0a0a92387ad329ea696ff2
Tomoki Yoshizawa <tomoki-goalie@hotmail.com>
2024-03-19 11:32:24 -0700

    add path

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const corsOptions = {
   optionsSuccessStatus: 200,
 };
 const app = express();
+const path = require("path");
 require("dotenv").config();
 
 const authRouter = require("./routes/authRoute");
@@ -25,6 +26,7 @@ app.use(express.json());
 // app.use(express.static("public"));
 app.use("/images", express.static(path.join(__dirname, "public/images")));
 
+// app.use(cors());
 app.use(cors(corsOptions));
 
 app.use("/auth", authRouter);
